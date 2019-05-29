eebde8d0f5a4c34e77d597d10f931c4ed0bba12f
JohnVidal77 <ba.john.vidal@gmail.com>
2019-05-29 01:32:11 -0300

    login mecanic done v0.5

--- app/login/login.controller.js
+++ app/login/login.controller.js
@@ -11,5 +11,21 @@
      
      function LoginController(){
          const vm = this;
+
+         vm.logoUrl = "https://media.wired.com/photos/5926838bcefba457b079a3ce/master/pass/slack-lead1.jpg";
+
+         vm.onClick_login = onClick_login;
+
+         function onClick_login() {
+             if(!vm.emailLogin){
+                 return
+             }
+
+             if(!vm.passwordLogin){
+                 return
+             }
+
+             console.log("Success on login");
+         }
      }
  })();
