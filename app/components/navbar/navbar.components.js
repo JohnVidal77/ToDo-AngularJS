745692a096d0534b4e0c186fbe389385a147d0e5
JohnVidal77 <ba.john.vidal@gmail.com>
2019-05-31 14:19:41 -0300

    overlay css

--- app/components/navbar/navbar.components.js
+++ app/components/navbar/navbar.components.js
@@ -20,21 +20,37 @@
         vm.openNotification = openNotification;
         vm.closeSide = closeSide;
         vm.closeNotification = closeNotification;
+        vm.showOverlay = showOverlay;
+        vm.hideOverlay = hideOverlay;
 
         function openSide() {
             document.getElementById("Sidenav").style.left = "0";
+            vm.showOverlay();
         }
 
         function openNotification() {
             document.getElementById("Notification").style.right = "0";
+            vm.showOverlay();
         }
 
         function closeSide() {
             document.getElementById("Sidenav").style.left = "-100%";
+            vm.hideOverlay();
         }
 
         function closeNotification() {
             document.getElementById("Notification").style.right = "-100%";
+            vm.hideOverlay();
+        }
+
+        function showOverlay() {
+            document.getElementById("Overlay").style.top = "0";
+            document.getElementById("Overlay").style.opacity = "0.5";
+        }
+
+        function hideOverlay() {
+            document.getElementById("Overlay").style.top = "-100%";
+            document.getElementById("Overlay").style.opacity = "0";
         }
     }
 })();
