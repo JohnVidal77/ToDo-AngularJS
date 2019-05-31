9a5ebd8de447e3b03fdf9b14c41e02b316b1ffb4
JohnVidal77 <ba.john.vidal@gmail.com>
2019-05-31 12:41:21 -0300

    notification animation v0.5

--- app/components/navbar/navbar.components.js
+++ app/components/navbar/navbar.components.js
@@ -16,15 +16,25 @@
         console.log(window.__env);
 
         vm.onClick_ChangeRoute = () => serviceRoute.changeRoute(sessionStorage.getItem('last-state'));
-        vm.openNav = openNav;
+        vm.openSide = openSide;
+        vm.openNotification = openNotification;
         vm.closeSide = closeSide;
+        vm.closeNotification = closeNotification;
 
-        function openNav() {
+        function openSide() {
             document.getElementById("Sidenav").style.left = "0";
+        }
+
+        function openNotification() {
+            document.getElementById("Notification").style.right = "0";
         }
 
         function closeSide() {
             document.getElementById("Sidenav").style.left = "-100%";
+        }
+
+        function closeNotification() {
+            document.getElementById("Notification").style.right = "-100%";
         }
     }
 })();
