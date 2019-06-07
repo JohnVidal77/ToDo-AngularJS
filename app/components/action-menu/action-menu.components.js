78312ae32aa2504b4b6d5d297afa76c53ce089d2
JohnVidal77 <ba.john.vidal@gmail.com>
2019-06-07 11:25:18 -0300

    tooltip action menu ready

--- app/components/action-menu/action-menu.components.js
+++ app/components/action-menu/action-menu.components.js
@@ -21,8 +21,12 @@
                 document.getElementById('ActionMenu').style.transform = "rotate(45deg)";
                 document.getElementById('BtnCreateTask').style.bottom = "140px";
                 document.getElementById('BtnCreateGoal').style.bottom = "90px";
+                document.getElementsByClassName('tooltip-action-menu')[0].style.width='100px';
+                document.getElementsByClassName('tooltip-action-menu')[1].style.width='100px';
                 vm.actived = true;
             }else{
+                document.getElementsByClassName('tooltip-action-menu')[0].style.width='0';
+                document.getElementsByClassName('tooltip-action-menu')[1].style.width='0';
                 document.getElementById('ActionMenu').style.transform = "rotate(0deg)";
                 document.getElementById('BtnCreateTask').style.bottom = "32px";
                 document.getElementById('BtnCreateGoal').style.bottom = "32px";
