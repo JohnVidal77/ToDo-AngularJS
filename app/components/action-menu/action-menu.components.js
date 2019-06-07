c1a24b388c2761aebf39fca06c4d76dda2d34b63
JohnVidal77 <ba.john.vidal@gmail.com>
2019-06-07 02:08:47 -0300

    action-menu mobile css v0.5

--- app/components/action-menu/action-menu.components.js
+++ app/components/action-menu/action-menu.components.js
@@ -11,5 +11,24 @@
 
     function ActionMenuController($state){
         const vm = this;
+
+        vm.onClick_activeMenu = onClick_activeMenu;
+
+        vm.actived = false;
+
+        function onClick_activeMenu() {
+            if(!vm.actived) {
+                document.getElementById('ActionMenu').style.transform = "rotate(45deg)";
+                document.getElementById('BtnCreateTask').style.bottom = "140px";
+                document.getElementById('BtnCreateGoal').style.bottom = "90px";
+                vm.actived = true;
+            }else{
+                document.getElementById('ActionMenu').style.transform = "rotate(0deg)";
+                document.getElementById('BtnCreateTask').style.bottom = "32px";
+                document.getElementById('BtnCreateGoal').style.bottom = "32px";
+                vm.actived = false;
+            }
+
+        }
     }
 })();
